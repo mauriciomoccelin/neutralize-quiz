78af3d2114b4cdd505c38fe25b5e95cd1d7cf211
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2021-10-07 10:30:42 -0300

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://root:root@localhost:27017'),
+      mongoose.connect('mongodb://root:root@mongo:27017'),
   },
 ];
