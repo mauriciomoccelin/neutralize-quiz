9703c467bea59fcf6b16876ae0872e34cff48ddc
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2021-10-13 10:07:07 -0300

    Add the base for e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,45 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { genereteUserDto } from './fixture/test-fixture';
+
+const userToRegister = genereteUserDto();
+const userToLogin = {
+  username: userToRegister.username,
+  password: userToRegister.password,
+};
 
 describe('AppController (e2e)', () => {
+  let access_token: string;
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    await request(app.getHttpServer())
+      .post('/app/users/register')
+      .send(userToRegister)
+      .expect(201);
+
+    const response = await request(app.getHttpServer())
+      .post('/app/auth/login')
+      .send(userToLogin)
+      .set('Accept', 'application/json')
+      .expect(201);
+
+    access_token = response.body.access_token;
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  test('/ (GET)', () => {
+    console.log(access_token);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
--- test/fixture/test-fixture.ts
+++ test/fixture/test-fixture.ts
@@ -1,3 +1,14 @@
-import {} from 'faker';
+import { internet, name, random } from 'faker';
 
-export const genereteUserDto;
+import { Role } from '../../src/modules/roles/role.enum';
+import { UserDto } from '../../src/modules/users/dto/user.dto';
+
+export const roles: Role[] = [Role.Admin, Role.User];
+
+export const genereteUserDto = (): UserDto => ({
+  name: name.firstName() + ' ' + name.lastName(),
+  email: internet.email(),
+  password: internet.password(),
+  username: internet.userName(),
+  roles: random.arrayElements(roles),
+});
