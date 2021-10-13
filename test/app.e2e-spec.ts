ab763c732d2c49109a4b19fb11d6941ba8500055
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2021-10-13 17:09:28 -0300

    Add e2e test for /app/quizzes/my/get-by-id api

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
 import { AppModule } from './../src/app.module';
+import { QuizDto } from './../src/modules/quiz/dto/quiz.dto';
+
 import { genereteUserDto, generetSaveQuizDto } from './fixture/test-fixture';
 
 const userToRegister = genereteUserDto();
@@ -48,6 +51,25 @@ describe('AppController (e2e)', () => {
   test('(GET) /app/quizzes/my/get-all', async () => {
     const response = await request(app.getHttpServer())
       .get('/app/quizzes/my/get-all')
+      .set('Authorization', `Bearer ${access_token}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  test('(GET) /app/quizzes/my/get-by-id', async () => {
+    const responseGetAll = await request(app.getHttpServer())
+      .get('/app/quizzes/my/get-all')
+      .set('Authorization', `Bearer ${access_token}`);
+
+    expect(responseGetAll.status).toBe(200);
+    const quizzes = responseGetAll.body as QuizDto[];
+    const quiz = quizzes[0];
+
+    expect(quiz).not.toBeNull();
+
+    const response = await request(app.getHttpServer())
+      .get('/app/quizzes/my/get-by-id')
+      .query({ id: quiz._id })
       .set('Authorization', `Bearer ${access_token}`);
 
     expect(response.status).toBe(200);
